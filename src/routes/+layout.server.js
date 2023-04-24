99365be78a1b9777bdf819a8d4176a125e0ef7ad
Benito Anagua <benito.anagua@gmail.com>
2023-04-24 10:32:38 -0400

    show error logs

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,15 +2,15 @@ import GhostContentAPI from "@tryghost/content-api";
 import { error } from "@sveltejs/kit";
 // import { GHOST_URL, GHOST_API_KEY } from "$env/static/private";
 
-const api = new GhostContentAPI({
-  url: "https://elnacional.link",
-  key: "fff2aa11a8f827564141b78e9d",
-  version: "v5.0",
-});
-
 export async function load() {
+  const api = new GhostContentAPI({
+    url: "https://elnacional.link",
+    key: "fff2aa11a8f827564141b78e9d",
+    version: "v5.0",
+  });
   const tags = await api.tags.browse().catch((e) => {
-    throw error(e.status, e.message);
+    throw error(e);
+    console.error(e);
   });
   return {
     tags,
